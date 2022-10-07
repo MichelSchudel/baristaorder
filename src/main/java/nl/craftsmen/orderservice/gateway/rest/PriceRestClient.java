07fb2c83c9d633dbcaef1e11545e3b36572adb3e
Michel Schudel <michel.schudel@gmail.com>
2022-10-07 11:01:15 +0200

    more fixes

--- src/main/java/nl/craftsmen/orderservice/gateway/rest/PriceRestClient.java
+++ src/main/java/nl/craftsmen/orderservice/gateway/rest/PriceRestClient.java
@@ -2,12 +2,13 @@ package nl.craftsmen.orderservice.gateway.rest;
 
 import nl.craftsmen.orderservice.core.PriceProvider;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import static java.util.Objects.requireNonNull;
 
-@Service
+@Repository
 public class PriceRestClient implements PriceProvider {
 
     private final RestTemplate restTemplate;
